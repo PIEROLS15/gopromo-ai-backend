3afa839a61afcce108ef773563a1185589f893b8
PIEROLS15 <pierodanielllanossanchez@gmail.com>
2026-01-25 17:49:08 -0500

    feat: api education levels

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,16 @@ import { ContactModule } from './contact/contact.module';
 import { AuthModule } from './auth/auth.module';
 import { LocationsModule } from './locations/locations.module';
 import { SuppliersModule } from './suppliers/suppliers.module';
+import { EducationLevelsModule } from './education-levels/education-levels.module';
 
 @Module({
-  imports: [ContactModule, AuthModule, LocationsModule, SuppliersModule],
+  imports: [
+    ContactModule,
+    AuthModule,
+    LocationsModule,
+    SuppliersModule,
+    EducationLevelsModule,
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
